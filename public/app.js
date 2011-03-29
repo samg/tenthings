84dab2fc8d9cea1f1a08e9e9c0ab1146ce9354f7
Sam Goldstein <sam@aboutus.org>
2011-03-28 21:33:22 -0700

    Ajax uploader

--- public/app.js
+++ public/app.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
+    var createUploaders = function (){
+    $('.image div').each(function(){
+      var uploader = new qq.FileUploader({
+        element: this,
+        action: 'do-nothing.htm',
+        debug: true
+      });
+    });
+  };
+
   $('.edit').live('click', function(){
+    createUploaders();
     $(this).removeClass('edit').addClass('view');
     $('.editable').each(
       function(){
@@ -17,6 +28,7 @@ $(document).ready(function(){
   $('.view').live('click', function(){
     $('body').removeClass('editBG');
     $(this).removeClass('view').addClass('edit');
+    $('.image .uploader').html('');
     $('.editable').each(
       function(){
         $(this).html($(this).find('input, textarea').val());
