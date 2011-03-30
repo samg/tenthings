3068ac31564250d445f1b2a314626a0df9e3f853
Sam Goldstein <sam@aboutus.org>
2011-03-29 18:24:25 -0700

    Rails 3.0.5 skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TenthingsOrg::Application.config.secret_token = '6e0f90f41c9c23f000ac04741b339244e710bb166195e48b0092eba49c1eaa5fe039d5a671a0862901a8decc25ce7e8edc51d5a4751339c84c3d93b26df5950d'
--- public/compile.sh
+++ public/compile.sh
@@ -1,6 +1,6 @@
 while [ true ] ; do
  haml home.html.haml > .home;
- mv .home home;
+ mv .home index.html;
  sleep 1;
  date;
 done
