082596676b73ca3499a0a8113554182121ce96e4
Sam Goldstein <sam@aboutus.org>
2011-04-03 22:44:33 -0700

    Controller skeleton

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,24 @@
 class ListsController < ApplicationController
+
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -2,4 +2,25 @@ require 'spec_helper'
 
 describe ListsController do
 
+  describe "#index" do
+  end
+
+  describe "#create" do
+  end
+
+  describe "#new" do
+  end
+
+  describe "#show" do
+  end
+
+  describe "#edit" do
+  end
+
+  describe "#update" do
+  end
+
+  describe "#destroy" do
+  end
+
 end
